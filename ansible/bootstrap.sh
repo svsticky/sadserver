1f6ddce6392d7926864172505967c438bf5415fa
Laurens Duijvesteijn <l.duijvesteijn@gmail.com>
2016-08-24 16:16:10 +0200

    New bootstrap script for new hosts

---
+++ ansible/bootstrap.sh
@@ -0,0 +1,65 @@
+#!/bin/sh
+
+##
+## This bootstrap script sets up a vanilla Ubuntu 16.04 image, so that it can
+## run our ansible playbooks.
+##
+## It contains the absolute bare minimum to get a host up and running. Before
+## adding new features to this script, consider if it is possible to add these
+## to the playbook itself.
+##
+
+display_error() {
+  echo >&2 $1
+}
+
+# Check if $1, which is the first argument of our script
+# is empty/not set
+if [ -z "${1+set}" ]; then
+  display_error "usage: ./bootstrap.sh <HOSTNAME | IP>"
+  exit 1
+fi
+
+# SSH specifications for the different users
+SSH_HOST=$1
+SSH_SPEC=root@${SSH_HOST}
+
+# Password that will be set for the ansible user
+PASSWORD=$(openssl rand -base64 15)
+
+echo "==> Bootstrapping host '${1}'"
+
+echo "==> Installing Python 2.7"
+ssh -T $SSH_SPEC > /dev/null << EOC
+apt-get update
+apt-get install -y --no-install-recommends python2.7
+EOC
+
+echo "==> Creating ansible user"
+ssh -T $SSH_SPEC > /dev/null << EOC
+useradd -d /home/ansible -s /bin/bash ansible -G sudo
+mkdir /home/ansible
+chown ansible:ansible /home/ansible
+echo "ansible:${PASSWORD}" | chpasswd
+EOC
+
+echo "==> Copying root ssh keys to ansible user"
+ssh -T $SSH_SPEC > /dev/null << EOC
+cp -r /root/.ssh /home/ansible/
+chown ansible:ansible -R /home/ansible/.ssh
+EOC
+
+echo "==> Sucessfully bootstrapped host"
+
+cat << EOM
+
+Now edit the hosts file to contain ${SSH_HOST} and run the main playbook
+by executing:
+
+  ansible-playbook -i hosts main.yml --ask-become-pass
+
+It will attempt to run the main playbook under the ansible user we've just
+created. The user ansible has the password ${PASSWORD}
+
+Don't forget to add this to the IT Crowd password manager.
+EOM

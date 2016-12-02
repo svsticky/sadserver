7f4793a1e665b98e542cd3acd6fedcfcfbcdec40
Maarten van den Berg <maartenberg1@gmail.com>
2016-12-02 15:20:22 +0100

    Drop koala-test folder

--- koala-test/files/webrick.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-PATH="/home/koala/.rbenv/bin:$PATH"
-eval "$(rbenv init -)"
-
-bundle exec rails server -b 0.0.0.0
--- koala-test/templates/koala-unicorn.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-PATH="/home/koala/.rbenv/bin:$PATH"
-eval "$(rbenv init -)"
-
-bundle exec unicorn -c config/unicorn.rb -E {{ koala_environment }} -D

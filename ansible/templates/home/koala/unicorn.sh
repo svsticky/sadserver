65d35ed5884ddbebabcdb051073d26833e1467e0
Tom Wassenberg <tom@tomwassenberg.nl>
2016-12-27 22:26:58 +0100

    Made canonical hostname variable in entire playbook
    - Changed all references which contain hostnames to use the existing variable
    - Changed files that contain hostnames to templates
    - Removed canonical hostname from filenames which contain hostnames

--- ansible/templates/home/koala/unicorn.sh
+++ ansible/templates/home/koala/unicorn.sh
@@ -3,5 +3,5 @@
 PATH="/home/koala/.rbenv/bin:$PATH"
 eval "$(rbenv init -)"
 
-cd /var/www/koala.svsticky.nl
+cd /var/www/koala.{{ canonical_hostname }}
 bundle exec unicorn -c config/unicorn.rb -E {{ koala_environment }} -D

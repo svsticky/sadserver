c886badbcebfd7b7a49a05c466e91dc2663262e2
Tom Wassenberg <tomwassenberg@users.noreply.github.com>
2017-03-24 21:48:43 +0100

    Add notifications for runs and extra info in MOTD
    - Adds playbook revision (commit hash) to the server's MOTD
    - Cleans up not necessary info from MOTD
    - Adds notifications to Slack at the beginning and end of each
    deployment, which adds slacktee as a local dependency

--- ansible/scripts/run-playbook.sh
+++ ansible/scripts/run-playbook.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
-set -e
-ANSIBLE_SSH_PIPELINING=true ansible-playbook -i hosts --ask-become-pass $@
+
+GIT_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+GIT_REVISION="$(git rev-parse HEAD)"
+GIT_ROOT="$(git rev-parse --show-toplevel)"
+alias slacktee="slacktee.sh --config '${GIT_ROOT}/ansible/templates/etc/slacktee.conf' --plain-text --username 'Ansible'"
+
+echo "*Deployment of playbook \"_$1_\" started by ${USER}*\n_(branch: ${GIT_BRANCH} - revision \"${GIT_REVISION}\")_" | slacktee --icon ':construction:' --attachment '#46c4ff' > /dev/null
+
+ANSIBLE_SSH_PIPELINING=true ansible-playbook -i "${GIT_ROOT}/ansible/hosts" --ask-become-pass --extra-vars "playbook_revision=${GIT_REVISION}" "$@"
+
+if [ "$?" = "0" ]; then
+  echo "*Deployment of playbook \"_$1_\" successfully completed*\n_(branch: ${GIT_BRANCH} - revision \"${GIT_REVISION}\")_" | slacktee --icon ':ok_hand:' --attachment 'good' > /dev/null
+else
+  echo "*Deployment of playbook \"_$1_\" FAILED!*\n_(branch: ${GIT_BRANCH} - revision \"${GIT_REVISION}\")_" | slacktee --icon ':exclamation:' --attachment 'danger' > /dev/null
+fi

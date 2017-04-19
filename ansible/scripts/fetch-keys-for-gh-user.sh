2b49a74a49c67052990541e0dc59974a2c5cf59e
Laurens Duijvesteijn <l.duijvesteijn@gmail.com>
2017-04-19 20:15:40 +0200

    Add a script to automate fetching GH keys

---
+++ ansible/scripts/fetch-keys-for-gh-user.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+## This script is a utility to fetch SSH public keys from a GitHub
+## account and add them to the proper directory.
+##
+## USAGE: fetch-keys-for-gh-user.sh GITHUB_USERNAME
+
+# Strict mode
+set -efuo pipefail
+
+# Find the absolute path to the skyblue repository
+SKYBLUE_DIR=$(git rev-parse --show-toplevel)
+
+# Check if a GH username was given
+if [[ -z "$1" ]]; then
+  >&2 echo "USAGE: fetch-keys-for-gh-user.sh GITHUB_USERNAME"
+  exit 1
+fi
+
+CREDENTIALS_PATH="${SKYBLUE_DIR}/ansible/credentials/ssh/$1.github.pub"
+curl "https://github.com/$1.keys" > "${CREDENTIALS_PATH}"

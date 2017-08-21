6d3bb0d5e348682521b942bfcff12e6d8a673083
Tom Wassenberg <tom@tomwassenberg.nl>
2017-08-21 14:56:52 +0200

    Fix variable check in fetch-keys script

--- ansible/scripts/fetch-keys-for-gh-user.sh
+++ ansible/scripts/fetch-keys-for-gh-user.sh
@@ -10,13 +10,13 @@ set -eEfuo pipefail
 IFS=$'\n\t'
 
 # Find the absolute path to the sadserver repository
-SADSERVER_DIR=$(git rev-parse --show-toplevel)
+SADSERVER_DIR="$(git rev-parse --show-toplevel)"
 
 # Check if a GH username was given
-if [[ -z "$1" ]]; then
+if [[ -z "${1+x}" ]]; then
   >&2 echo "USAGE: fetch-keys-for-gh-user.sh GITHUB_USERNAME"
   exit 1
 fi
 
-CREDENTIALS_PATH="${SADSERVER_DIR}/ansible/credentials/ssh/$1.github.pub"
-curl --fail "https://github.com/$1.keys" > "${CREDENTIALS_PATH}"
+CREDENTIALS_PATH="${SADSERVER_DIR}/ansible/credentials/ssh/${1}.github.pub"
+curl --fail "https://github.com/${1}.keys" > "${CREDENTIALS_PATH}"

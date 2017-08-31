4a9709f475e795a646de392452307c689b548692
Tom Wassenberg <tom@tomwassenberg.nl>
2017-08-31 19:00:53 +0200

    Make fetch-keys script more robust
    - Add error handling in case of typo/unaware username change
    - Use curl's -o flag instead of redirecting the output, so it never creates an
    empty file

--- ansible/scripts/fetch-keys-for-gh-user.sh
+++ ansible/scripts/fetch-keys-for-gh-user.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-## This script is a utility to fetch SSH public keys from a GitHub
-## account and add them to the proper directory.
+## This script is a utility to fetch SSH public keys from a GitHub account and
+## add them to the proper directory.
 ##
 ## USAGE: fetch-keys-for-gh-user.sh GITHUB_USERNAME
 
@@ -19,4 +19,17 @@ if [[ -z "${1+x}" ]]; then
 fi
 
 CREDENTIALS_PATH="${SADSERVER_DIR}/ansible/credentials/ssh/${1}.github.pub"
-curl --fail "https://github.com/${1}.keys" > "${CREDENTIALS_PATH}"
+# Temporarily disable halting on errors, to manually catch possible curl errors
+set +e
+curl --fail "https://github.com/${1}.keys" -o "${CREDENTIALS_PATH}" > \
+/dev/null 2>&1
+
+if [[ "${?}" != "0" ]]; then
+  echo "Fetching keys failed. Did you enter a correct GitHub username?"
+  exit 1
+fi
+# Re-enable halting on errors
+set -e
+
+AMOUNT_OF_KEYS="$(wc -l "${CREDENTIALS_PATH}" | awk '{print $1}')"
+echo "${AMOUNT_OF_KEYS} Key(s) fetched successfully for ${1}."

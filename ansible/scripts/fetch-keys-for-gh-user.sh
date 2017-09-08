20a687de943f55a9f0b18b34592bf9c584882caa
Tom Wassenberg <tom@tomwassenberg.nl>
2017-09-08 18:49:17 +0200

    Make fetch-keys script handle multiple usernames
    Also properly handle accounts without any keys associated to them. If such an
    account is the only one supplied, exit with error code 1.

--- ansible/scripts/fetch-keys-for-gh-user.sh
+++ ansible/scripts/fetch-keys-for-gh-user.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-## This script is a utility to fetch SSH public keys from a GitHub account and
+## This script is a utility to fetch SSH public keys from GitHub accounts and
 ## add them to the proper directory.
 ##
-## USAGE: fetch-keys-for-gh-user.sh GITHUB_USERNAME
+## USAGE: fetch-keys-for-gh-user.sh [GITHUB_USERNAME]...
 
 # Unofficial Bash strict mode
 set -eEfuo pipefail
@@ -12,24 +12,35 @@ IFS=$'\n\t'
 # Find the absolute path to the sadserver repository
 SADSERVER_DIR="$(git rev-parse --show-toplevel)"
 
-# Check if a GH username was given
+# Check if a minimum of 1 GH username was given
 if [[ -z "${1+x}" ]]; then
-  >&2 echo "USAGE: fetch-keys-for-gh-user.sh GITHUB_USERNAME"
+  >&2 echo "USAGE: fetch-keys-for-gh-user.sh [GITHUB_USERNAME]..."
   exit 1
 fi
 
-CREDENTIALS_PATH="${SADSERVER_DIR}/ansible/credentials/ssh/${1}.github.pub"
-# Temporarily disable halting on errors, to manually catch possible curl errors
-set +e
-curl --fail "https://github.com/${1}.keys" -o "${CREDENTIALS_PATH}" > \
-/dev/null 2>&1
+for USERNAME in "${@}"; do
+  CREDENTIALS_PATH="${SADSERVER_DIR}/ansible/credentials/ssh/${USERNAME}.github.pub"
+  # Temporarily disable halting on errors, to manually catch possible curl
+  # errors
+  set +e
+  curl --fail "https://github.com/${USERNAME}.keys" -o "${CREDENTIALS_PATH}" > \
+  /dev/null 2>&1
 
-if [[ "${?}" != "0" ]]; then
-  echo "Fetching keys failed. Did you enter a correct GitHub username?"
-  exit 1
-fi
-# Re-enable halting on errors
-set -e
+  if [[ "${?}" != "0" ]]; then
+    echo "Fetching keys failed. Did you enter a correct GitHub username?"
+    exit 1
+  fi
+  # Re-enable halting on errors
+  set -e
 
-AMOUNT_OF_KEYS="$(wc -l "${CREDENTIALS_PATH}" | awk '{print $1}')"
-echo "${AMOUNT_OF_KEYS} Key(s) fetched successfully for ${1}."
+  AMOUNT_OF_KEYS="$(wc -l "${CREDENTIALS_PATH}" | awk '{print $1}')"
+  if [[ "${AMOUNT_OF_KEYS}" -gt 0 ]]; then
+    echo "${AMOUNT_OF_KEYS} Key(s) fetched successfully for ${USERNAME}."
+  else
+    rm "${CREDENTIALS_PATH}"
+    echo "No keys found in GitHub account of ${USERNAME}."
+    if [[ "$#" -eq 1 ]]; then
+      exit 1
+    fi
+  fi
+done

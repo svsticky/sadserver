3c39dbf77803a0044014b5dc0224d5b5b10ef3c9
Hugo <hpeters1024@gmail.com>
2022-04-04 22:16:43 +0200

    add staging/production switch

--- ansible/plugins/vars/bw_plugin.py
+++ ansible/plugins/vars/bw_plugin.py
@@ -3,6 +3,12 @@ from ansible.plugins.vars import BaseVarsPlugin  # type:ignore[import]
 from functools import lru_cache
 import subprocess
 import json
+import os
+
+folder_ids = {
+    "production" : "b690c7cf-16bb-4b69-b86a-ae6d01449634",
+    "staging": "a4c144b8-457d-45d4-b3a9-ae51014bed02",
+}
 
 # Utility class that allows dot syntax on dict objects
 class DictObj:
@@ -22,14 +28,20 @@ def extract_info(item):
     return { name: fields }
 
 def parse_bw_output(output):
-    return list(map(extract_info, json.loads(output)))
+     items = list(map(extract_info, json.loads(output)))
+     ret = {}
+     for k in items:
+        print(k)
+     return ret
 
-@lru_cache(maxsize=1000)
+@lru_cache(maxsize=100000)
 def global_get_vars():
-    print("Hi from bw plugin")
-    output = parse_bw_output(subprocess.check_output(["bw list items --folderid a4c144b8-457d-45d4-b3a9-ae51014bed02"], shell=True))
-    print(output[1])
-    return {'secret_hello': DictObj(output[1]) }
+    host = os.environ["STICKY_ENV"]
+    folder_id = folder_ids[host]
+    print(f"Fetching credentials from bitwarden for {host} (folder: {folder_id})")
+    secrets = parse_bw_output(subprocess.check_output([f"bw list items --folderid {folder_id}"], shell=True))
+    print(secrets)
+    return {'secrets': DictObj(secrets) }
 
 
 

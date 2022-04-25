68aaaa23707fdc93937aae98b6f34a15e55f6a6d
Hugo <hpeters1024@gmail.com>
2022-04-25 21:31:54 +0200

    migrate staging secrets for koala and payconiq

--- ansible/plugins/vars/bw_plugin.py
+++ ansible/plugins/vars/bw_plugin.py
@@ -4,21 +4,12 @@ from functools import lru_cache
 import subprocess
 import json
 import os
+import inspect
 
 folder_ids = {
     "production" : "b690c7cf-16bb-4b69-b86a-ae6d01449634",
     "staging": "a4c144b8-457d-45d4-b3a9-ae51014bed02",
 }
-
-# Utility class that allows dot syntax on dict objects
-class DictObj:
-    def __init__(self, in_dict:dict):
-        assert isinstance(in_dict, dict)
-        for key, val in in_dict.items():
-            if isinstance(val, (list, tuple)):
-               setattr(self, key, [DictObj(x) if isinstance(x, dict) else x for x in val])
-            else:
-               setattr(self, key, DictObj(val) if isinstance(val, dict) else val)
 
 def extract_info(item):
     name = item["name"]
@@ -31,7 +22,7 @@ def parse_bw_output(output):
      items = list(map(extract_info, json.loads(output)))
      ret = {}
      for k in items:
-        print(k)
+         ret = {**ret, **k}
      return ret
 
 @lru_cache(maxsize=100000)
@@ -40,8 +31,9 @@ def global_get_vars():
     folder_id = folder_ids[host]
     print(f"Fetching credentials from bitwarden for {host} (folder: {folder_id})")
     secrets = parse_bw_output(subprocess.check_output([f"bw list items --folderid {folder_id}"], shell=True))
-    print(secrets)
-    return {'secrets': DictObj(secrets) }
+    ret = {'secrets': secrets }
+    print(ret)
+    return ret
 
 
 
